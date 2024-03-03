50f9d2207cf52d26f0437563f70f7bf82932ad50
Ender-267 <albertolozano2004@gmail.com>
2024-03-03 03:48:27 +0100

    me voy a volar la cabeza con una escopeta recortada

--- p1_tabla.sql
+++ p1_tabla.sql
@@ -1,117 +1,17 @@
-DROP producto;
-DROP formatos_comercializacion:
-DROP tamaño_formato;
-DROP articulos;
-DROP compras;
-DROP pedidos;
-DROP proveedor;
-DROP datos_tarjeta;
-DROP datos_direccion;
-DROP cliente_generico;
-DROP cliente_registrado;
-DROP publicaciones;
-DROP oferta;
-DROP descuentos;
-
-CREATE TABLE producto (
-    nombre VARCHAR2(256) NOT NULL,
-    coffea VARCHAR2(32) NOT NULL,
-    varietal VARCHAR2(32) NOT NULL,
-    pais_origen VARCHAR2(64) NOT NULL,
-    tipo_tostado ENUM('natural','torrfacto','mezcla') NOT NULL,
-    descafeinado BOOLEAN NOT NULL
-    CONSTRAINT pk_producto PRIMARY KEY(nombre)
-);
-
-CREATE TABLE formatos_comercializacion(
-    producto VARCHAR2(256) NOT NULL
-    tipo ENUM('grano crudo','grano tostado','molido','liofilizado','capsulas','preparado') NOT NULL,
-    CONSTRAINT pk_formatos_comercializacion PRIMARY KEY (producto, tipo),
-    CONSTRAINT fk_producto FOREIGN KEY producto REFERENCES producto(nombre)
-);
-
-CREATE TABLE tamaño_formato(
-    formato VARCHAR2(256) NOT NULL,
-    cantidad NUMBER(10) NOT NULL,
-    definicion_cantidad ENUM('volumen','peso') NOT NULL,
-    CONSTRAINT pk_tamaño_formato PRIMARY KEY(formato, cantidad, definicion_cantidad),
-    CONSTRAINT ck_cantidad CHECK (cantidad>0),
-    CONSTRAINT fk_formato FOREIGN KEY formato REFERENCES formatos_comercializacion(producto)
-);
-
-CREATE TABLE articulos(
-    producto VARCHAR2(256) NOT NULL,
-    codigo_barras NUMBER(16) NOT NULL,
-    descripcion VARCHAR2(256) NOT NULL,
-    envalaje_tamaño NUMBER(8) NOT NULL,
-    precio_venta NUMBER(8,2) NOT NULL,
-    stock NUMBER(8) DEFAULT 0 NOT NULL,
-    min_stock NUMBER(8) DEFAULT 5 NOT NULL,
-    max_stock NUMBER(8) DEFAULT 15 NOT NULL,
-    comprado NUMBER(16) DEFAULT NULL ON DELETE SET NULL,
-    CONSTRAINT pk_articulos PRIMARY KEY (codigo_barras)
-    CONSTRAINT ck_envalaje_tamaño CHECK (envalaje_tamaño>=0),
-    CONSTRAINT ck_precio_venta CHECK (precio_venta>=0),
-    CONSTRAINT ck_stock CHECK (stock>=min_stock AND stock<=max_stock),
-    CONSTRAINT ck_min_stock CHECK (min_stock>=0),
-    CONSTRAINT ck_max_stock CHECK (max_stock>=0),
-    CONSTRAINT fk_producto FOREIGN KEY producto REFERENCES producto(nombre),
-    CONSTRAINT fk_envalaje_tamaño FOREIGN KEY envalaje_tamaño REFERENCES tamaño_formato(cantidad),
-
-);
-
-CREATE TABLE compras(
-    cliente NUMBER(16) NOT NULL
-    fecha DATE NOT NULL,
-    direccion_entrega NUMBER(16) NOT NULL,
-    tipo_facturacion ENUM('COD', 'banco', 'tarjeta') NOT NULL,
-    datos_tarjeta NUMBER(16),
-    fecha_pago DATE,
-    fecha_entrega DATE,
-    precio_total NUMBER(8,2) DEFAULT 0 NOT NULL,
-    CONSTRAINT pk_compras PRIMARY KEY (fecha, direccion_entrega),
-    CONSTRAINT fk_cliente FOREIGN KEY cliente REFERENCES cliente_generico(id),
-    CONSTRAINT ck_precio_total CHECK (precio_total>=0),
-    CONSTRAINT ck_tarjeta 
-        CHECK ((tipo_facturacion='tajeta' AND datos_tarjeta = NULL AND fecha=fecha_entrega) 
-        OR (tipo_facturacion != 'tarjeta' AND datos_tarjeta = NULL))
-);
-
-CREATE TABLE pedidos(
-    producto VARCHAR2(256) NOT NULL,
-    fecha DATE NOT NULL,
-    estado ENUM('fullfulled', 'placed', 'draft') NOT NULL,
-    unidades NUMBER(8) NOT NULL,
-    proveedor VARCHAR2(64) ON DELETE SET NULL,
-    fecha_recepcion DATE,
-    precio_total NUMBER(8,2),
-
-    CONSTRAINT pk_pedidos PRIMARY KEY (articulos),
-    CONSTRAINT fk_producto FOREIGN KEY (producto) REFERENCES producto(nombre),
-    CONSTRAINT fk_proveedor FOREIGN KEY (proveedor) REFERENCES proveedor(cif),
-    CONSTRAINT ck_unidades CHECK (unidades>0),
-    CONSTRAINT ck_precio_total CHECK (precio_total>0)
-    CONSTRAINT ck_draft CHECK ((estado != 'draft') OR (estado = 'draft' AND proveedor = NULL)),
-    CONSTRAINT ck_fullfiled
-        CHECK ((estado != 'fullfiled')
-        OR (estado = 'fullfiled' AND fecha_recepcion != NULL AND fecha_recepcion>fecha AND precio_total != NULL))
-);
-
-CREATE TABLE proveedores(
-    nombre_registrado VARCHAR2(256) UNIQUE NOT NULL,
-    cif NUMBER(32) NOT NULL,
-    nombre_completo VARCHAR(256) UNIQUE NOT NULL,
-    correo_electronico VARCHAR2(32) UNIQUE NOT NULL,
-    numero_bancario NUMBER(32) UNIQUE NOT NULL,
-    direccion_postal NUMBER(16) NOT NULL,
-    pais VARCHAR2(32) NOT NULL
-    tiempo_medio NUMBER(4, 2) DEFAULT 0,
-    pedidos_satisfechos NUMBER(4) DEFAULT 0,
-    CONSTRAINT pk_proveedores PRIMARY KEY (cif)
-    CONSTRAINT fk_direccion_postal FOREIGN KEY datos_direccion(codigo_postal),
-    CONSTRAINT ck_tiempo_medio CHECK (tiempo_medio>=0),
-    CONSTRAINT ck_pedidos_satisfechos CHECK (pedidos_satisfechos>=0)
-);
+DROP TABLE productos CASCADE CONSTRAINT;
+DROP TABLE formatos_comercializacion CASCADE CONSTRAINT;
+DROP TABLE tamaño_formato CASCADE CONSTRAINT;
+DROP TABLE articulos CASCADE CONSTRAINT;
+DROP TABLE compras CASCADE CONSTRAINT;
+DROP TABLE pedidos CASCADE CONSTRAINT;
+DROP TABLE proveedores CASCADE CONSTRAINT;
+DROP TABLE datos_tarjeta CASCADE CONSTRAINT;
+DROP TABLE datos_direccion CASCADE CONSTRAINT;
+DROP TABLE cliente_generico CASCADE CONSTRAINT;
+DROP TABLE cliente_registrado CASCADE CONSTRAINT;
+DROP TABLE publicaciones CASCADE CONSTRAINT;
+DROP TABLE oferta CASCADE CONSTRAINT;
+DROP TABLE descuentos CASCADE CONSTRAINT;
 
 CREATE TABLE cliente_generico(
     id NUMBER(8) NOT NULL,
@@ -120,46 +20,85 @@ CREATE TABLE cliente_generico(
     nombre VARCHAR2(16),
     apellido1 VARCHAR2(16),
     apellido2 VARCHAR2(16),
-    UNIQUE (nombre, apellido1, apellido2)
+    UNIQUE (nombre, apellido1, apellido2),
+    CONSTRAINT pk_cliente_generico PRIMARY KEY (id)
 );
 
 CREATE TABLE cliente_registrado(
     id_cliente_generico NUMBER(8) NOT NULL,
     fecha_registro DATE NOT NULL,
-    telefono NUMBER(16),
-    correo_electronico VARCHAR2(32),
-    nombre VARCHAR2(16) NOT NULL,
-    apellido1 VARCHAR2(16) NOT NULL,
-    apellido2 VARCHAR2(16),
-    preferencia_contacto 
-        ENUM('email', 'telefono', 'sms', 'whatsapp', 'facebook', 'wechat', 'qqmobile', 'snapchat', 'telegram') 
-        NOT NULL 
-        DEFAULT(
-            CASE 
-                WHEN telefono = NULL THEN 'email'
-                ELSE 'sms'
-            END
-        ),
+    preferencia_contacto VARCHAR2(16) NOT NULL,
     CONSTRAINT pk_clienteregistrado PRIMARY KEY (id_cliente_generico),
-    CONSTRAINT fk_id_cliente_generico FOREIGN KEY id_cliente_generico REFERENCES cliente_generico(id),
-    CONSTRAINT fk_telefono FOREIGN KEY telefono REFERENCES cliente_generico(telefono),
-    CONSTRAINT fk_correo_electronico FOREIGN KEY correo_electronico REFERENCES cliente_generico(correo_electronico),
-    CONSTRAINT fk_nombre FOREIGN KEY nombre REFERENCES cliente_generico(nombre),
-    CONSTRAINT fk_apellido1 FOREIGN KEY apellido1 REFERENCES cliente_generico(apellido1),
-    CONSTRAINT fk_apellido2 FOREIGN KEY apellido2 REFERENCES cliente_generico(apellido2),
+    CONSTRAINT fk_id_cliente_generico FOREIGN KEY (id_cliente_generico) REFERENCES cliente_generico(id)
 );
 
-CREATE TABLE datos_tarjeta(
-    titular VARCHAR2(128) NOT NULL,
-    compañia VARCHAR2(128) NOT NULL,
-    numero NUMBER(16) NOT NULL,
-    fecha_vencimiento DATE NOT NULL,
-    cliente NUMBER(8),
-    CONSTRAINT pk_numero PRIMARY KEY (numero),
-    CONSTRAINT fk_cliente FOREIGN KEY cliente_registrado(id_cliente_generico)
+CREATE TABLE productos(
+    nombre VARCHAR2(256) NOT NULL,
+    coffea VARCHAR2(32) NOT NULL,
+    varietal VARCHAR2(32) NOT NULL,
+    pais_origen VARCHAR2(64) NOT NULL,
+    tipo_tostado VARCHAR2(16) NOT NULL,
+    descafeinado VARCHAR2(1) NOT NULL,
+    CONSTRAINT pk_productos PRIMARY KEY(nombre)
+);
+
+CREATE TABLE formatos_comercializacion(
+    producto VARCHAR2(256) NOT NULL,
+    tipo VARCHAR2(16) NOT NULL,
+    CONSTRAINT pk_formatos_comercializacion PRIMARY KEY (producto, tipo),
+    CONSTRAINT fk_formatos_comercializacion FOREIGN KEY (producto) REFERENCES productos(nombre)
+);
+
+CREATE TABLE tamaño_formato(
+    formato VARCHAR2(256) NOT NULL,
+    tipo VARCHAR2(16) NOT NULL,
+    cantidad NUMBER(10) NOT NULL,
+    definicion_cantidad VARCHAR2(16) NOT NULL,
+    CONSTRAINT pk_tamano_formato PRIMARY KEY(formato, cantidad, definicion_cantidad),
+    CONSTRAINT ck_cantidad CHECK (cantidad>0),
+    CONSTRAINT fk_formato FOREIGN KEY (formato, tipo) REFERENCES formatos_comercializacion(producto, tipo)
+);
+
+CREATE TABLE compras(
+    cliente NUMBER(16) NOT NULL,
+    fecha DATE NOT NULL,
+    direccion_entrega NUMBER(16) NOT NULL,
+    tipo_facturacion VARCHAR2(16) NOT NULL,
+    datos_tarjeta NUMBER(16),
+    fecha_pago DATE,
+    fecha_entrega DATE,
+    precio_total NUMBER(8,2) DEFAULT 0 NOT NULL,
+    CONSTRAINT pk_compras PRIMARY KEY (fecha, direccion_entrega),
+    CONSTRAINT fk_cliente FOREIGN KEY (cliente) REFERENCES cliente_generico(id),
+    CONSTRAINT ck_precio_total CHECK (precio_total>=0),
+    CONSTRAINT ck_tarjeta 
+        CHECK (((tipo_facturacion='tarjeta' AND datos_tarjeta IS NULL) AND fecha=fecha_entrega) 
+        OR (tipo_facturacion != 'tarjeta' AND datos_tarjeta IS NULL))
+);
+
+CREATE TABLE articulos(
+    producto VARCHAR2(256) NOT NULL,
+    codigo_barras NUMBER(16) NOT NULL,
+    descripcion VARCHAR2(256) NOT NULL,
+    cantidad NUMBER(8) NOT NULL,
+    precio_venta NUMBER(8,2) NOT NULL,
+    stock NUMBER(8) DEFAULT 0 NOT NULL,
+    min_stock NUMBER(8) DEFAULT 5 NOT NULL,
+    max_stock NUMBER(8) DEFAULT 15 NOT NULL,
+    comprado_fecha DATE DEFAULT NULL,
+	comprado_direccion NUMBER(16) DEFAULT NULL,
+    CONSTRAINT pk_articulos PRIMARY KEY (codigo_barras),
+    CONSTRAINT ck_cantidad2 CHECK (cantidad>=0),
+    CONSTRAINT ck_precio_venta CHECK (precio_venta>=0),
+    CONSTRAINT ck_stock CHECK (stock>=min_stock AND stock<=max_stock),
+    CONSTRAINT ck_min_stock CHECK (min_stock>=0),
+    CONSTRAINT ck_max_stock CHECK (max_stock>=0),
+    CONSTRAINT fk_producto2 FOREIGN KEY (producto) REFERENCES productos(nombre),
+    CONSTRAINT fk_comprado FOREIGN KEY (comprado_fecha, comprado_direccion) REFERENCES compras(fecha, direccion_entrega) ON DELETE SET NULL
 );
 
 CREATE TABLE datos_direccion(
+    coordenadas VARCHAR2(32) NOT NULL,
     tipo_via VARCHAR2(16) NOT NULL,
     nombre_via VARCHAR2(32) NOT NULL,
     numero_inmueble NUMBER(3),
@@ -171,11 +110,52 @@ CREATE TABLE datos_direccion(
     ciudad VARCHAR2(16) NOT NULL,
     pais VARCHAR2(16) NOT NULL,
     cliente NUMBER(8),
-    CONSTRAINT pk_datos_direccion 
-        PRIMARY KEY (tipo_via, nombre_via, numero_inmueble, numero_bloque, escalera, piso, puerta, codigo_postal, ciudad, pais),
-    CONSTRAINT fk_cliente FOREIGN KEY (cliente) REFERENCES cliente_registrado(id_cliente_generico)
+    CONSTRAINT pk_datos_direccion PRIMARY KEY (coordenadas),
+    CONSTRAINT fk_cliente2 FOREIGN KEY (cliente) REFERENCES cliente_registrado(id_cliente_generico)
 );
 
+CREATE TABLE proveedores(
+    nombre_registrado VARCHAR2(256) UNIQUE NOT NULL,
+    cif NUMBER(32) NOT NULL,
+    nombre_completo VARCHAR(256) UNIQUE NOT NULL,
+    correo_electronico VARCHAR2(32) UNIQUE NOT NULL,
+    numero_bancario NUMBER(32) UNIQUE NOT NULL,
+    direccion VARCHAR2(32) NOT NULL,
+    pais VARCHAR2(32) NOT NULL,
+    tiempo_medio NUMBER(4, 2) DEFAULT 0,
+    pedidos_satisfechos NUMBER(4) DEFAULT 0,
+    CONSTRAINT pk_proveedores PRIMARY KEY (cif),
+    CONSTRAINT fk_direccion FOREIGN KEY (direccion) REFERENCES datos_direccion(coordenadas),
+    CONSTRAINT ck_tiempo_medio CHECK (tiempo_medio>=0),
+    CONSTRAINT ck_pedidos_satisfechos CHECK (pedidos_satisfechos>=0)
+);
+
+CREATE TABLE pedidos (
+    producto VARCHAR2(256) NOT NULL,
+    fecha DATE NOT NULL,
+    estado VARCHAR2(16) NOT NULL,
+    unidades NUMBER(8) NOT NULL,
+    proveedor NUMBER(32),
+    fecha_recepcion DATE,
+    precio_total NUMBER(8,2),
+    CONSTRAINT pk_pedidos PRIMARY KEY (producto),
+    CONSTRAINT fk_producto FOREIGN KEY (producto) REFERENCES productos(nombre),
+    CONSTRAINT fk_proveedor FOREIGN KEY (proveedor) REFERENCES proveedores(cif) ON DELETE SET NULL,
+    CONSTRAINT ck_unidades CHECK (unidades > 0),
+    CONSTRAINT ck_precio_total2 CHECK (precio_total > 0),
+    CONSTRAINT ck_draft CHECK ((estado != 'draft') OR (estado = 'draft' AND proveedor IS NULL)),
+    CONSTRAINT ck_fullfiled CHECK ((estado != 'fulfilled') OR ((estado = 'fulfilled' AND fecha_recepcion IS NOT NULL) AND (fecha_recepcion > fecha AND precio_total IS NOT NULL))) -- Fixed condition and OR usage
+);
+
+CREATE TABLE datos_tarjeta(
+    titular VARCHAR2(128) NOT NULL,
+    compañia VARCHAR2(128) NOT NULL,
+    numero NUMBER(16) NOT NULL,
+    fecha_vencimiento DATE NOT NULL,
+    cliente NUMBER(8),
+    CONSTRAINT pk_numero PRIMARY KEY (numero),
+    CONSTRAINT fk_cliente3 FOREIGN KEY (cliente) REFERENCES cliente_registrado(id_cliente_generico)
+);
 
 CREATE TABLE publicaciones(
     id NUMBER(8) NOT NULL,
@@ -183,12 +163,12 @@ CREATE TABLE publicaciones(
     articulos NUMBER(16),
     puntuacion NUMBER(1) NOT NULL,
     likes NUMBER(5) NOT NULL,
-    refrenda BOOLEAN NOT NULL,
-    cliente NUMBER(8) ON DELETE SET NULL,
+    refrenda VARCHAR(1) NOT NULL,
+    cliente NUMBER(8),
     CONSTRAINT pk_publicaciones PRIMARY KEY(id),
-    CONSTRAINT fk_producto FOREIGN KEY producto REFERENCES productos(nombre),
-    CONSTRAINT fk_articulos FOREIGN KEY articulos REFERENCES articulos(codigo_barras),
-    CONSTRAINT fk_cliente FOREIGN KEY client REFERENCES cliente_registrado(id_cliente_generico),
+    CONSTRAINT fk_producto3 FOREIGN KEY (producto) REFERENCES productos(nombre),
+    CONSTRAINT fk_articulos FOREIGN KEY (articulos) REFERENCES articulos(codigo_barras),
+    CONSTRAINT fk_cliente4 FOREIGN KEY (cliente) REFERENCES cliente_registrado(id_cliente_generico) ON DELETE SET NULL,
     CONSTRAINT ck_puntuacion CHECK(puntuacion<=5 AND puntuacion>=1),
     CONSTRAINT ck_likes CHECK(likes>=0)
 );
@@ -202,11 +182,11 @@ CREATE TABLE descuentos(
 );
 
 CREATE TABLE oferta(
-    proveedor NUMBER(32) ON DELETE CASCADE NOT NULL,
-    producto VARCHAR2(256) ON DELETE CASCADE NOT NULL,
+    proveedor NUMBER(32) NOT NULL,
+    producto VARCHAR2(256) NOT NULL,
     precio NUMBER(8,2),
     CONSTRAINT pk_oferta PRIMARY KEY (proveedor, producto),
-    CONSTRAINT fk_proveedor FOREIGN KEY proveedor REFERENCES proveedores(nombre),
-    CONSTRAINT fk_producto FOREIGN KEY producto REFERENCES producto(producto),
+    CONSTRAINT fk_proveedor2 FOREIGN KEY (proveedor) REFERENCES proveedores(cif) ON DELETE CASCADE,
+    CONSTRAINT fk_producto4 FOREIGN KEY (producto) REFERENCES productos(nombre) ON DELETE CASCADE,
     CONSTRAINT ck_precio CHECK (precio>0)
 );
