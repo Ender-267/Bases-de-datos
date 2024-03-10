ddbccf3e95474dc68366b449b11c1a8cbb5190df
Ender-267 <albertolozano2004@gmail.com>
2024-03-10 19:13:02 +0100

    m

--- p1_tabla.sql
+++ p1_tabla.sql
@@ -1,5 +1,7 @@
 CLEAR SCREEN;
 
+DROP SEQUENCE coordenadas_seq;
+DROP SEQUENCE cliente_generico_id_seq;
 DROP TABLE producto CASCADE CONSTRAINT;
 DROP TABLE formato_comercializacion CASCADE CONSTRAINT;
 DROP TABLE tamaño_formato CASCADE CONSTRAINT;
@@ -17,20 +19,19 @@ DROP TABLE descuento CASCADE CONSTRAINT;
 
 CREATE TABLE cliente_generico(
     id NUMBER(8) NOT NULL,
-    telefono NUMBER(16),
-    correo_electronico VARCHAR2(32),
-    nombre VARCHAR2(16),
-    apellido1 VARCHAR2(16),
-    apellido2 VARCHAR2(16),
-    UNIQUE (nombre, apellido1, apellido2),
+    telefono NUMBER(9),
+    correo_electronico VARCHAR2(60),
+    nombre VARCHAR2(35),
+    apellido1 VARCHAR2(30),
+    apellido2 VARCHAR2(30),
     CONSTRAINT pk_cliente_generico PRIMARY KEY (id)
 );
 
-CREATE TABLE cliente_registrado(
+CREATE TABLE cliente_registrado (
     id_cliente_generico NUMBER(8) NOT NULL,
     nombre_usuario VARCHAR2(64) NOT NULL UNIQUE,
-    fecha_registro DATE NOT NULL,
-    preferencia_contacto VARCHAR2(16) NOT NULL,
+    fecha_registro VARCHAR2(28) NOT NULL,
+    preferencia_contacto VARCHAR2(16),
     CONSTRAINT pk_clienteregistrado PRIMARY KEY (id_cliente_generico),
     CONSTRAINT fk_id_cliente_generico FOREIGN KEY (id_cliente_generico) REFERENCES cliente_generico(id)
 );
@@ -45,10 +46,27 @@ CREATE TABLE producto(
     CONSTRAINT pk_producto PRIMARY KEY(nombre)
 );
 
+CREATE TABLE datos_direccion(
+    coordenadas VARCHAR2(32) NOT NULL,
+    tipo_via VARCHAR2(16) NOT NULL,
+    nombre_via VARCHAR2(32) NOT NULL,
+    numero_inmueble VARCHAR2(3),
+    numero_bloque VARCHAR2(3),
+    escalera VARCHAR2(2),
+    piso VARCHAR2(7),
+    puerta VARCHAR2(3),
+    codigo_postal NUMBER(5) NOT NULL,
+    ciudad VARCHAR2(45) NOT NULL,
+    pais VARCHAR2(45) NOT NULL,
+    cliente NUMBER(8),
+    CONSTRAINT pk_datos_direccion PRIMARY KEY (coordenadas),
+    CONSTRAINT fk_cliente2 FOREIGN KEY (cliente) REFERENCES cliente_registrado(id_cliente_generico)
+);
+
 CREATE TABLE compra(
-    cliente NUMBER(16) NOT NULL,
+    cliente NUMBER(8) NOT NULL,
     fecha DATE NOT NULL,
-    direccion_entrega NUMBER(16) NOT NULL,
+    direccion_entrega VARCHAR2(32) NOT NULL,
     tipo_facturacion VARCHAR2(16) NOT NULL,
     datos_tarjeta NUMBER(16),
     fecha_pago DATE,
@@ -56,6 +74,7 @@ CREATE TABLE compra(
     precio_total NUMBER(8,2) DEFAULT 0 NOT NULL,
     CONSTRAINT pk_compras PRIMARY KEY (fecha, direccion_entrega),
     CONSTRAINT fk_cliente FOREIGN KEY (cliente) REFERENCES cliente_generico(id),
+    CONSTRAINT fk_direccion_entrega3 FOREIGN KEY (direccion_entrega) REFERENCES datos_direccion(coordenadas),
     CONSTRAINT ck_precio_total CHECK (precio_total>=0),
     CONSTRAINT ck_tarjeta 
         CHECK (((tipo_facturacion='tarjeta' AND datos_tarjeta IS NULL) AND fecha=fecha_entrega) 
@@ -64,15 +83,15 @@ CREATE TABLE compra(
 
 CREATE TABLE articulo(
     producto VARCHAR2(50) NOT NULL,
-    codigo_barras NUMBER(15) NOT NULL,
+    codigo_barras VARCHAR2(15) NOT NULL,
+    embalaje_recipiente VARCHAR2(8) NOT NULL,
     embalaje_tamaño NUMBER(8) NOT NULL,
-    embalaje_tipo VARCHAR2(4) NOT NULL,
+    embalaje_unidad VARCHAR2(8) NOT NULL,
     precio_venta NUMBER(14,2) NOT NULL,
     stock NUMBER(5) DEFAULT 0 NOT NULL,
     min_stock NUMBER(5) DEFAULT 5 NOT NULL,
     max_stock NUMBER(5) DEFAULT 15 NOT NULL,
     tipo_formato VARCHAR2(20) NOT NULL,
-    definicion_cantidad VARCHAR2(16) NOT NULL,
     comprado_fecha DATE DEFAULT NULL,
 	comprado_direccion NUMBER(16) DEFAULT NULL,
     CONSTRAINT pk_articulos PRIMARY KEY (codigo_barras),
@@ -85,45 +104,28 @@ CREATE TABLE articulo(
     CONSTRAINT fk_comprado FOREIGN KEY (comprado_fecha, comprado_direccion) REFERENCES compra(fecha, direccion_entrega) ON DELETE SET NULL
 );
 
-CREATE TABLE datos_direccion(
-    coordenadas VARCHAR2(32) NOT NULL,
-    tipo_via VARCHAR2(16) NOT NULL,
-    nombre_via VARCHAR2(32) NOT NULL,
-    numero_inmueble NUMBER(3),
-    numero_bloque NUMBER(3),
-    escalera VARCHAR2(2),
-    piso NUMBER(2),
-    puerta VARCHAR2(2),
-    codigo_postal NUMBER(16) NOT NULL,
-    ciudad VARCHAR2(16) NOT NULL,
-    pais VARCHAR2(16) NOT NULL,
-    cliente NUMBER(8),
-    CONSTRAINT pk_datos_direccion PRIMARY KEY (coordenadas),
-    CONSTRAINT fk_cliente2 FOREIGN KEY (cliente) REFERENCES cliente_registrado(id_cliente_generico)
-);
-
 CREATE TABLE proveedor(
-    nombre_registrado VARCHAR2(256) UNIQUE NOT NULL,
-    cif NUMBER(32) NOT NULL,
-    nombre_completo VARCHAR(256) UNIQUE NOT NULL,
-    correo_electronico VARCHAR2(32) UNIQUE NOT NULL,
-    numero_bancario NUMBER(32) UNIQUE NOT NULL,
-    direccion VARCHAR2(32) NOT NULL,
-    pais VARCHAR2(32) NOT NULL,
+    nombre_registrado VARCHAR2(35) UNIQUE NOT NULL,
+    cif VARCHAR2(10) NOT NULL,
+    nombre_completo VARCHAR(90) UNIQUE NOT NULL,
+    correo_electronico VARCHAR2(60) UNIQUE NOT NULL,
+    telefono VARCHAR2(9) UNIQUE NOT NULL,
+    numero_bancario NUMBER(30),
+    direccion VARCHAR2(120) NOT NULL,
+    pais VARCHAR2(45) NOT NULL,
     tiempo_medio NUMBER(4, 2) DEFAULT 0,
     pedidos_satisfechos NUMBER(4) DEFAULT 0,
     CONSTRAINT pk_proveedores PRIMARY KEY (cif),
-    CONSTRAINT fk_direccion FOREIGN KEY (direccion) REFERENCES datos_direccion(coordenadas),
     CONSTRAINT ck_tiempo_medio CHECK (tiempo_medio>=0),
     CONSTRAINT ck_pedidos_satisfechos CHECK (pedidos_satisfechos>=0)
 );
 
 CREATE TABLE pedido (
-    producto VARCHAR2(256) NOT NULL,
+    producto VARCHAR2(10) NOT NULL,
     fecha DATE NOT NULL,
     estado VARCHAR2(16) NOT NULL,
     unidades NUMBER(8) NOT NULL,
-    proveedor NUMBER(32),
+    proveedor VARCHAR2(10),
     fecha_recepcion DATE,
     precio_total NUMBER(8,2),
     CONSTRAINT pk_pedidos PRIMARY KEY (producto),
@@ -147,16 +149,16 @@ CREATE TABLE datos_tarjeta(
 
 CREATE TABLE publicacion(
     id NUMBER(8) NOT NULL,
-    producto VARCHAR2(256) NOT NULL,
-    articulo NUMBER(16),
+    producto VARCHAR2(50) NOT NULL,
+    articulo VARCHAR2(16),
     puntuacion NUMBER(1) NOT NULL,
     likes NUMBER(5) NOT NULL,
     refrenda VARCHAR(1) NOT NULL,
-    cliente NUMBER(8),
+    cliente VARCHAR2(30),
     CONSTRAINT pk_publicaciones PRIMARY KEY(id),
     CONSTRAINT fk_producto3 FOREIGN KEY (producto) REFERENCES producto(nombre),
     CONSTRAINT fk_articulos FOREIGN KEY (articulo) REFERENCES articulo(codigo_barras),
-    CONSTRAINT fk_cliente4 FOREIGN KEY (cliente) REFERENCES cliente_registrado(id_cliente_generico) ON DELETE SET NULL,
+    CONSTRAINT fk_cliente4 FOREIGN KEY (cliente) REFERENCES cliente_registrado(nombre_usuario) ON DELETE SET NULL,
     CONSTRAINT ck_puntuacion CHECK(puntuacion<=5 AND puntuacion>=1),
     CONSTRAINT ck_likes CHECK(likes>=0)
 );
@@ -170,11 +172,182 @@ CREATE TABLE descuento(
 );
 
 CREATE TABLE oferta(
-    proveedor NUMBER(32) NOT NULL,
-    producto VARCHAR2(256) NOT NULL,
+    proveedor VARCHAR2(10) NOT NULL,
+    producto VARCHAR2(50) NOT NULL,
     precio NUMBER(8,2),
     CONSTRAINT pk_oferta PRIMARY KEY (proveedor, producto),
     CONSTRAINT fk_proveedor2 FOREIGN KEY (proveedor) REFERENCES proveedor(cif) ON DELETE CASCADE,
     CONSTRAINT fk_producto4 FOREIGN KEY (producto) REFERENCES producto(nombre) ON DELETE CASCADE,
-    CONSTRAINT ck_precio CHECK (precio>0)
+    CONSTRAINT ck_precio CHECK (precio>=0)
 );
+
+
+-- producto
+insert into producto(nombre, coffea, varietal, pais_origen, tipo_tostado, descafeinado)
+(select product, MAX(coffea), MAX(varietal), MAX(origin), MAX(roasting), MAX(decaf) FROM fsdb.catalogue GROUP BY product);
+
+-- atributo
+insert into articulo(codigo_barras, producto, embalaje_recipiente, embalaje_tamaño, embalaje_unidad, precio_venta, stock, min_stock, max_stock, tipo_formato)
+select
+        barcode,
+        MIN(product),
+        MAX(SUBSTR(packaging, 1, (INSTR(packaging, ' ', 1)))) AS recipiente,
+        MAX(TO_NUMBER(REGEXP_SUBSTR(packaging, '\S+', 1, 2))) AS tamano,
+        MAX(REPLACE(SUBSTR(packaging, INSTR(packaging, ' ', 5) + 1) , ' ', '')) AS units, --ns porque es 5 pero funciona asi
+        MAX(TO_NUMBER(REPLACE(SUBSTR(retail_price, 1, INSTR(retail_price, ' ',1)-1), '.', ','))) AS precio, 
+        MAX(TO_NUMBER(cur_stock)) AS cstock, 
+        MAX(TO_NUMBER(min_stock)) AS minstock, 
+        MAX(TO_NUMBER(max_stock)) AS maxstock, 
+        MAX(format) as tipo_formato
+        FROM fsdb.catalogue GROUP BY barcode;
+-- packaging => pkg  <numero> <unidades>.
+-- price => uu.dd ?
+-- max_stock/min_stock/cur_stock => numero
+
+-- proveedor
+insert into proveedor(nombre_registrado, cif, nombre_completo, correo_electronico, telefono, pais, direccion)
+select supplier, 
+        MAX(prov_taxid),
+        MAX(prov_person),
+        MAX(prov_email),
+        MAX(prov_mobile),
+        MAX(prov_country),
+        MAX(prov_address)
+FROM fsdb.catalogue 
+WHERE prov_taxid IS NOT NULL
+GROUP BY supplier;
+
+---> inserción de clientes genéricos
+CREATE SEQUENCE cliente_generico_id_seq START WITH 1 INCREMENT BY 1;
+
+INSERT INTO cliente_generico
+(id, telefono, correo_electronico, nombre, apellido1, apellido2)
+SELECT 
+    cliente_generico_id_seq.NEXTVAL,
+    client_mobile,
+    client_email, 
+    client_name, 
+    client_surn1, 
+    client_surn2
+FROM fsdb.trolley;
+
+--- inserción de clientes registrado
+INSERT INTO cliente_registrado (id_cliente_generico, nombre_usuario, fecha_registro)
+SELECT MAX(t1.id), t2.username, MAX(TO_DATE(t2.reg_date, 'YYYY / MM / DD'))
+FROM cliente_generico t1
+JOIN fsdb.trolley t2 ON (t1.telefono = t2.client_mobile) AND (t1.correo_electronico = t2.client_email) AND (t1.nombre = t2.client_name) AND
+(t1.apellido1 = t2.client_surn1) AND (t1.apellido2 = t2.client_surn2)
+WHERE t2.username IS NOT NULL GROUP BY t2.username;
+
+
+-- direciones
+-- no podemos tener coordenadas reales asi q las invento
+CREATE SEQUENCE coordenadas_seq START WITH 1 INCREMENT BY 1;
+
+INSERT INTO datos_direccion
+(coordenadas, tipo_via, nombre_via, numero_inmueble, numero_bloque, escalera, piso, puerta, codigo_postal, ciudad, pais)
+SELECT 
+    coordenadas_seq.NEXTVAL, 
+    dliv_waytype, 
+    dliv_wayname, 
+    dliv_gate, 
+    TO_NUMBER(dliv_block), 
+    TO_NUMBER(dliv_stairw), 
+    dliv_floor, 
+    dliv_door, 
+    TO_NUMBER(dliv_zip),
+    dliv_town, 
+    dliv_country
+FROM 
+    fsdb.trolley
+WHERE 
+    dliv_waytype IS NOT NULL 
+    AND dliv_wayname IS NOT NULL 
+    AND dliv_zip IS NOT NULL 
+    AND dliv_town IS NOT NULL 
+    AND dliv_country IS NOT NULL;
+
+INSERT INTO datos_direccion
+(coordenadas, tipo_via, nombre_via, numero_inmueble, numero_bloque, escalera, piso, puerta, codigo_postal, ciudad, pais)
+SELECT 
+    coordenadas_seq.NEXTVAL, 
+    bill_waytype, 
+    bill_wayname, 
+    bill_gate, 
+    TO_NUMBER(bill_block), 
+    TO_NUMBER(bill_stairw), 
+    bill_floor, 
+    bill_door, 
+    TO_NUMBER(bill_zip),
+    bill_town, 
+    bill_country
+FROM 
+    fsdb.trolley
+WHERE 
+    bill_waytype IS NOT NULL 
+    AND bill_wayname IS NOT NULL 
+    AND bill_zip IS NOT NULL 
+    AND bill_town IS NOT NULL 
+    AND bill_country IS NOT NULL;
+
+INSERT INTO datos_direccion(cliente)
+SELECT t1.id
+FROM cliente_generico t1
+JOIN cliente_registrado t2 ON t1.id = t2.id_cliente_generico
+JOIN fsdb.trolley t3 ON t2.nombre_usuario = t3.username
+WHERE t2.nombre_usuario IS NOT NULL
+
+INSERT INTO oferta(proveedor, producto, precio)
+SELECT 
+    prov_taxid,
+    product,
+    REPLACE(SUBSTR(MIN(cost_price), 1, INSTR(MIN(cost_price), ' ',1)-1), '.', ',')
+FROM 
+    fsdb.catalogue
+WHERE supplier IS NOT NULL AND product IS NOT NULL
+GROUP BY prov_taxid, product;
+
+
+INSERT INTO pedido (producto, fecha, estado, unidades, proveedor, fecha_recepcion, precio_total)
+SELECT 
+    t3.nombre,
+    TO_DATE(t1.orderdate, 'DD / MM / YYYY'), 
+    CASE
+        WHEN MAX(t4.proveedor) IS NULL THEN 'draft'
+        WHEN MAX(t1.payment_date) IS NOT NULL THEN 'fulfilled'
+        ELSE 'placed'
+    END,
+    SUM(TO_NUMBER(t1.quantity)),
+    t4.proveedor,
+    MIN(TO_DATE(t1.payment_date, 'DD / MM / YYYY')), 
+    SUM(t4.precio * TO_NUMBER(t1.quantity))
+FROM fsdb.trolley t1 
+JOIN articulo t2 ON t1.prodtype = t2.tipo_formato
+JOIN producto t3 ON t2.producto = t3.nombre AND t1.coffea = t3.coffea AND t1.varietal = t3.varietal
+JOIN oferta t4 ON t3.nombre = t4.producto
+WHERE t1.prodtype IS NOT NULL
+GROUP BY t4.proveedor, t3.nombre, t1.orderdate, t1.payment_date
+
+INSERT INTO datos_tarjeta(titular, compañia, numero, fecha_vencimiento)
+SELECT 
+    MAX(card_holder),
+    MAX(card_company),
+    card_number,
+    MAX(TO_DATE(card_expiratn, 'MM/YY'))
+FROM fsdb.trolley
+WHERE card_number IS NOT NULL
+GROUP BY card_number;
+
+INSERT INTO compra(cliente, fecha, direccion_entrega, tipo_facturacion, datos_tarjeta, fecha_pago, fecha_entrega, precio_total)
+SELECT 
+    t2.id
+    TO_DATE(t1.orderdate, 'DD / MM / YYYY'),
+
+
+
+
+
+-- @C:\users\aulavirtual\Desktop\script.sql
+    
+
+
